f39c59789903ee75bfc02f184f2d0260126ef040
Chari Nehal Dattaram (RBEI/EBB1) <NehalDattaram.Chari@in.bosch.com>
2020-05-20 22:02:42 +0530

    cmake install command

---
+++ cmake/module7/build/math_dir/CMakeFiles/my_math_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_math_lib.dir/src/add.cpp.o"
+  "CMakeFiles/my_math_lib.dir/src/div.cpp.o"
+  "libmy_math_lib.pdb"
+  "libmy_math_lib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_math_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
